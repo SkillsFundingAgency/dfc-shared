0b8871c79b817a29167623c592cd8168c120827a
Richard Lee <ric@dicci.co.uk>
2018-09-25 11:59:45 +0100

    Verbose output for ps script

--- azure/Set-SharedSettings.ps1
+++ azure/Set-SharedSettings.ps1
@@ -2,4 +2,4 @@ param (
     [Parameter(Mandatory=$true)][string]$KeyvaultName
 )
 
-Set-AzureRmKeyVaultAccessPolicy -VaultName $KeyvaultName -ServicePrincipalName abfa0a7c-a6b6-4736-8310-5855508787cd -PermissionsToSecrets get
+Set-AzureRmKeyVaultAccessPolicy -VaultName $KeyvaultName -ServicePrincipalName "abfa0a7c-a6b6-4736-8310-5855508787cd" -PermissionsToSecrets get -Verbose
