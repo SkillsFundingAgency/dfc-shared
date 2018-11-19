98553f01f4ed7a183fa3296382fe8ce9f14949ea
Richard Lee <ric@dicci.co.uk>
2018-11-19 12:38:58 +0000

    Recreating shared resources under new naming convention and agreed structure

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # dfc-shared
+
+Repository with all the shared Azure resources used across the DFC services.
+Each environment will have its own shared resources.
+ 
--- azure/Set-SharedSettings.ps1
+++
@@ -1,5 +0,0 @@
-param (
-    [Parameter(Mandatory=$true)][string]$KeyvaultName
-)
-
-Set-AzureRmKeyVaultAccessPolicy -VaultName $KeyvaultName -ServicePrincipalName "abfa0a7c-a6b6-4736-8310-5855508787cd" -PermissionsToSecrets get -Verbose
