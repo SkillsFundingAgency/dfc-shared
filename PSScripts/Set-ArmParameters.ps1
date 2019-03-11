dec343b4d735c76bdf7b75111af20e605c750041
NickGraham101 <nick.graham101@googlemail.com>
2019-03-11 15:21:47 +0000

    Added logging to Set-ArmParameters

--- PSScripts/Set-ArmParameters.ps1
+++ PSScripts/Set-ArmParameters.ps1
@@ -1,7 +1,9 @@
+[CmdletBinding()]
 param(
     [Parameter(Mandatory=$true)]
     [string]$Environment
 )
 
 $ApimExists = $null -ne (Get-AzureRmApiManagement -ResourceGroupName "dss-$Environment-shared-rg" -Name "dss-$Environment-shared-apim" -ErrorAction Ignore)
+Write-Verbose "Writing value $ApimExists to variable ApimExists"
 Write-Output "##vso[task.setvariable variable=ApimExists]$ApimExists"
