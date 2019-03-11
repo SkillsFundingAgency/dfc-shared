65fcd30823b927907874a4f0622d51de2b678893
NickGraham101 <nick.graham101@googlemail.com>
2019-03-11 15:51:16 +0000

    Corrected business area prefix

--- PSScripts/Set-ArmParameters.ps1
+++ PSScripts/Set-ArmParameters.ps1
@@ -4,6 +4,6 @@ param(
     [string]$Environment
 )
 
-$ApimExists = $null -ne (Get-AzureRmApiManagement -ResourceGroupName "dss-$Environment-shared-rg" -Name "dss-$Environment-shared-apim" -ErrorAction Ignore)
+$ApimExists = $null -ne (Get-AzureRmApiManagement -ResourceGroupName "dfc-$Environment-shared-rg" -Name "dfc-$Environment-shared-apim" -ErrorAction Ignore)
 Write-Verbose "Writing value $ApimExists to variable ApimExists"
 Write-Output "##vso[task.setvariable variable=ApimExists]$ApimExists"
