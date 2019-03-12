0950a64bc45ab614930bdaf675f26ea46d5bcfe0
Nick Graham <34914709+NickGraham101@users.noreply.github.com>
2019-03-12 11:49:15 +0000

    Ncsd 337 add condition to apim deployment (#10)
    * Added script & param to control ApimBasic deployment
    
    * Added logging to Set-ArmParameters
    
    * Added apimExists to parameters file
    
    * Corrected business area prefix
    
    * Set output to lower case
    
    * Corrected condition on sharedApimServiceBasic
    
    * Fixed syntax issue on sharedApimKeyVaultAccessPolicy
    
    * Made apim spn id ref conditional
    
    * Fixed apim spn reference
    
    * Fixed the right apim spn reference
    
    * Added missing dependsOn to sharedApimKeyVaultAccessPolicy
    
    * Change mode on key vault access policy copy

---
+++ PSScripts/Set-ArmParameters.ps1
@@ -0,0 +1,9 @@
+[CmdletBinding()]
+param(
+    [Parameter(Mandatory=$true)]
+    [string]$Environment
+)
+
+$ApimExists = $null -ne (Get-AzureRmApiManagement -ResourceGroupName "dfc-$Environment-shared-rg" -Name "dfc-$Environment-shared-apim" -ErrorAction Ignore)
+Write-Verbose "Writing value $ApimExists to variable ApimExists"
+Write-Output "##vso[task.setvariable variable=ApimExists]$($ApimExists.ToString().ToLower())"
