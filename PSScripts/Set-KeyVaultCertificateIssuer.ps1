efb2416aec69553b1d51d6840e7811fb3cfba080
NickGraham101 <nick.graham101@googlemail.com>
2019-11-27 14:04:01 +0000

    Added missing documentation

--- PSScripts/Set-KeyVaultCertificateIssuer.ps1
+++ PSScripts/Set-KeyVaultCertificateIssuer.ps1
@@ -5,25 +5,28 @@ Adds a Certificate Authority to an Azure KeyVault if it doesn't exist and sets t
 .DESCRIPTION
 Adds a Certificate Authority to an Azure KeyVault if it doesn't exist and sets the password.  The account or service principal executing the script will require SetIssuers, GetIssuers & ListIssuers on the KeyVault.
 
+.PARAMETER AdministratorPhoneNumber
+A phone number for the administrator, this is an enforced requirement of Set-AzKeyVaultCertificateIssuer
+
+.PARAMETER CertificateIssuerAccountId
+The GlobalSign account id assigned to careersdeveops@education.gov.uk
+
 .PARAMETER CertificateIssuerPassword
 The password for the GlobalSign account registered to careersdeveops@education.gov.uk
 
 .PARAMETER KeyVaultName
 The name of the KeyVault to add the CA to
-
-.PARAMETER CertificateIssuerAccountId
-The GlobalSign account id assigned to careersdeveops@education.gov.uk
 #>
 [CmdletBinding()]
 param(
     [Parameter(Mandatory=$true)]    
     [String]$AdministratorPhoneNumber,
     [Parameter(Mandatory=$true)]
+    [String]$CertificateIssuerAccountId,
+    [Parameter(Mandatory=$true)]
     [String]$CertificateIssuerPassword,    
     [Parameter(Mandatory=$true)]
-    [String]$KeyVaultName,
-    [Parameter(Mandatory=$true)]
-    [String]$CertificateIssuerAccountId
+    [String]$KeyVaultName
 )
 
 $AdminDetails = New-AzKeyVaultCertificateAdministratorDetails -FirstName Careers -LastName DevOps -EmailAddress "careersdeveops@education.gov.uk" -PhoneNumber $AdministratorPhoneNumber
